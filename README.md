f09929fc705bdb059b0d76a4addbe4b7a3742eaa
Jackson Reinagel <jreinagel4@gmail.com>
2020-04-22 18:47:39 -0600

    Update with filter and input line

--- README.md
+++ README.md
@@ -1,25 +1,14 @@
-# SPRINT OBJECTIVES
-Before you go any further, make sure you've done the following:
-> npm install
->> If you have issues installing webpack/start script see below
-
-You can spin up the front-end by using:
-> npm start
-
-You can spin up the back-end by using:
-> npm run server
-
 ### FRONT-END
-- Filter bugs by the threat level set by the user.
+-[x] Filter bugs by the threat level set by the user.
 
-- Create a modal that will allow users to submit new bugs. A bug report **must** contain:
+-[] Create a modal that will allow users to submit new bugs. A bug report **must** contain:
 
 >> 1. Assigned Team Member
 >> 2. Threat Level
 >> 3. Description
 >> 4. Reporter
 
-- Populate the client with bug reports sent back from the server.
+-[] Populate the client with bug reports sent back from the server.
 
 ### BACK-END
 - Create a route to send back **ALL** bug reports in the DB.
@@ -32,12 +21,3 @@ You can spin up the back-end by using:
 - Create a method to send back all bug reports in the DB.
 
 - Create a method to add a new bug report to the DB.
-
-### WEBPACK WOES
-
-If it continues to fail and you're on a macOS make sure you've installed Xcode:
-> xcode-select --install
->> npm install and try npm start again
-
-If it continues to fail: **globally install webpack, webpack-cli, and webpack-dev-server then:**
-> npm start
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,9 +16,16 @@ class App extends React.Component {
   }
 
   filterHandler(filter) {
-    this.setState({ filter });
+    this.setState({ filter }, () => this.updateBugList());
   }
 
+  updateBugList() {
+    let filtered = exampleData.filter(bug => {
+      return bug.threatLevel === this.state.filter;
+    });
+    this.setState({ bugs: filtered });
+  }
+  
   render() {
     return (
       <table>
@@ -36,6 +43,32 @@ class App extends React.Component {
             key={bug.bugName}
           />
         ))}
+        <tbody id="newBug">
+          <tr className="bugTile">
+            <td>{this.state.bugs.length + 1}</td>
+            <td><input type="text" placeholder="Describe the problem"></input></td>
+            <td><input type="text" placeholder="Your name"></input></td>
+            <td><input type="date"></input></td>
+            <td>
+              <select>
+                <option value="None"></option>
+                <option value="Bailey">Bailey</option>
+                <option value="Daniel">Daniel</option>
+                <option value="Surj">Surj</option>
+                <option value="Teddi">Teddi</option>
+              </select>
+            </td>
+            <td>
+              <select>
+                <option value="None"></option>
+                <option value="Low-Priority">Low-Priority</option>
+                <option value="Important">Important</option>
+                <option value="Critical">Critical</option>
+              </select>
+              <button className="btn">  Submit</button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     );
   }
